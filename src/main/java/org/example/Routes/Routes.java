0b9b2b444fe311613f08321acf4f930cb2f00722
ETHMUNI <sense1xnft@gmail.com>
2024-04-07 00:52:34 +0200

    logout function

--- src/main/java/org/example/Routes/Routes.java
+++ src/main/java/org/example/Routes/Routes.java
@@ -19,7 +19,8 @@ public class Routes {
                 post("/login", securityHandler.login(),Role.ANYONE);
                 post("/register", securityHandler.register(),Role.ANYONE);
                 before(securityHandler.authenticate());
-                post("/reset-password", securityHandler.resetPassword(), Role.USER);
+                post("/reset-password", securityHandler.resetPassword(), Role.USER, Role.ADMIN);
+                post("/logout", securityHandler.logout(), Role.USER, Role.ADMIN);
             });
         };
     }
